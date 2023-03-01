edeece61966c1cc208a5a080806b75e7084084e2
murtazamalik <murtazamalik1319@gmail.com>
2023-03-01 22:11:05 +0530

    Adding Employee Is Present Or Absent

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -3,5 +3,13 @@ package employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee Is Present");
+        }
+        else {
+            System.out.println("Employee Is Absent");
+        }
     }
 }
